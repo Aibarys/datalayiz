61b57c424f543667ebb26c5bf04150b4d6534256
ashaimur <aybarys@mail.ru>
2023-06-17 21:17:12 +0600

    update 17.06.23

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -27,8 +29,14 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post':post, 'form':form, 'sent':sent})
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+        
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -38,9 +46,24 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     # posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts})
+    return render(request, 'blog/post/list.html', {'page':page, 'posts':posts, 'tag':tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html',{'post':post})
+    
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, 'blog/post/detail.html',{'post':post, 
+                                                    'comments':comments, 
+                                                    'new_comment':new_comment, 
+                                                    'comment_form':comment_form})
 
